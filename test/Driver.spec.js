590073195aff8110e237f888d145ff0b028b55e5
James Benner <james.benner@gmail.com>
2019-02-18 14:16:27 -0500

    Added code coverage for Driver.

--- test/Driver.spec.js
+++ test/Driver.spec.js
@@ -5,4 +5,24 @@ describe('Driver', function () {
     it('is a function', function () {
         expect(Driver).to.be.a('function');
     });
+
+    context('#constructor', function () {
+        beforeEach(function () {
+            this.driver = new Driver('Driver Dan');
+        });
+
+        it('is a function', function () {
+            expect(Driver.prototype.constructor).to.be.a('function');
+        });
+
+        it('sets the name property', function () {
+            expect(this.driver.name).to.equal('Dan');
+        });
+
+        it('throws a TypeError if not instantiated with a valid record', function () {
+            const invalidInit = () => new Driver('Hello type error!');
+
+            expect(invalidInit).to.throw(TypeError);
+        });
+    });
 });
