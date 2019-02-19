66cb8c07457c80f72c3da19d0aee21207ac7fbfc
James Benner <james.benner@gmail.com>
2019-02-19 12:57:38 -0500

    Added Report#toArray method.

--- test/Report.spec.js
+++ test/Report.spec.js
@@ -1,5 +1,7 @@
 const { expect } = require('chai');
+const Driver = require('../lib/Driver');
 const Report = require('../lib/Report');
+const Trip = require('../lib/Trip');
 
 describe('Report', function () {
     beforeEach(function () {
@@ -52,6 +54,39 @@ describe('Report', function () {
             this.report.addDrivers(this.driversFixture);
 
             expect(this.report.drivers).to.eql(this.driversFixture);
+        });
+    });
+
+    context('#toArray', function () {
+        it('is a function', function () {
+            expect(Report.prototype.toArray).to.be.a('function');
+        });
+
+        it('returns an array', function () {
+            expect(this.report.toArray()).to.be.a('array');
+        });
+
+        it('returns an array of the formatted reports', function () {
+            const alex = new Driver('Driver Alex');
+            const bob = new Driver('Driver Bob');
+            const dan = new Driver('Driver Dan');
+            const alexsTrips = [
+                new Trip('Trip Alex 12:01 13:16 42.0')
+            ];
+            const dansTrips = [
+                new Trip('Trip Dan 07:15 07:45 17.3'),
+                new Trip('Trip Dan 06:12 06:32 21.8')
+            ];
+            const expected = [
+                'Alex: 42 miles @ 34 mph',
+                'Bob: 0 miles',
+                'Dan: 39 miles @ 47 mph'
+            ];
+
+            alex.addTrips(alexsTrips);
+            dan.addTrips(dansTrips);
+            this.report.addDrivers([alex, bob, dan]);
+            expect(this.report.toArray()).to.eql(expected);
         });
     });
 });
